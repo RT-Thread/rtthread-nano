30ba161180b60e5d5f0ee28d3f247c397529ef43
Bernard Xiong <bernard.xiong@gmail.com>
2018-10-12 12:11:07 +0800

    import rt-thread code
    update rt-thread version to 33cb916ff7aa684d06769497bb5f5914e7ff95a2

---
+++ rt-thread/components/libc/compilers/minilibc/errno.h
@@ -0,0 +1,8 @@
+#ifndef __ERRNO_H__
+#define __ERRNO_H__
+
+#include "libc/libc_errno.h"
+/* Others defined in libc/libc_errno.h */
+#define ENSRNOTFOUND 	163  	/* Domain name not found */
+
+#endif
