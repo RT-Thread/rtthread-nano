fb5b2b40ddeb29b1b704cb87c874ff16040f3fbb
chenchaoqun@rt-thread.com <chenchaoqun@rt-thread.com>
2018-11-22 18:00:31 +0800

    [synchronous] rt-thread 3.1.1 (date: 11.14)

--- rt-thread/components/libc/compilers/dlib/time.c
+++ rt-thread/components/libc/compilers/dlib/time.c
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2006-2018, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ */
 #include <sys/time.h>
 #include <rtthread.h>
 
@@ -33,10 +41,15 @@ int gettimeofday(struct timeval *tp, void *ignore)
  *
  */
 #pragma module_name = "?time"
-#if _DLIB_TIME_ALLOW_64
+#if _DLIB_TIME_USES_64
 time_t __time64(time_t *t)
 #else
+/* for IAR 6.2 later Compiler */
+#if defined (__IAR_SYSTEMS_ICC__) &&  (__VER__) >= 6020000
 time_t __time32(time_t *t)
+#else
+time_t time(time_t *t)
+#endif
 #endif
 {
   time_t time_now = 0;
